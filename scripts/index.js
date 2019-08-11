7fa9fae60bcb14cd6e07fbd9960763e307c9079d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-11 15:53:28 +0000

    Deploy xrr2016/blog to github.com/xrr2016/blog.git:gh-pages

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    // navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/sw.js')
   })
 }
 
+// theme
 const body = document.body
 const toggle = document.getElementById('toggle')
 const isDarkTheme = localStorage.getItem('isDarkTheme')
@@ -28,3 +29,41 @@ if (toggle) {
     }
   })
 }
+
+// comment
+const loader = document.getElementById('loader')
+const utteranc = document.getElementById('utteranc')
+
+if (utteranc) {
+  const uscript = document.createElement('script')
+
+  uscript.async = true
+  uscript.crossOrigin = 'anonymous'
+  uscript.src = 'https://utteranc.es/client.js'
+  uscript.setAttribute('repo', 'xrr2016/blog')
+  uscript.setAttribute('issue-term', 'pathname')
+
+  if (isDarkTheme === 'true') {
+    uscript.setAttribute('theme', 'github-dark')
+  } else {
+    uscript.setAttribute('theme', 'github-light')
+  }
+
+  utteranc.appendChild(uscript)
+
+  const timeout = setTimeout(function() {
+    loader.remove()
+    clearTimeout(timeout)
+  }, 3000)
+}
+
+// nav
+const path = location.pathname
+const navs = ['/', '/projects/', '/tags/', '/archives/', '/about/']
+const navLinkList = document.querySelectorAll('.nav-link')
+
+navs.forEach(function(nav, index) {
+  if (nav === path) {
+    navLinkList[index].classList.add('active')
+  }
+})
